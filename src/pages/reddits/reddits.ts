c034646f8f1992be826d7029a81182c84fc0d6a6
envy7 <noel.varghese.22@gmail.com>
2017-03-20 21:07:31 +0530

    main page working

--- src/pages/reddits/reddits.ts
+++ src/pages/reddits/reddits.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { RedditService } from '../../app/services/reddit.service';
 
 @Component({
   selector: 'reddits',
   templateUrl: 'reddits.html'
 })
 export class RedditsPage {
-
-  constructor(public navCtrl: NavController) {
+  items : any;
+  constructor(public navCtrl: NavController, private redditService: RedditService) {
 
   }
 
+  ngOnInit(){
+  	this.getPosts('sports', 10);
+  }
+
+  getPosts( category, limit ){
+  	this.redditService.getPosts(category, limit).subscribe(response => {
+  		this.items = response.data.children;
+  	})
+  }
 }
